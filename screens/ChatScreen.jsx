114844ad3bd62c05b0a515ec88214c5b466a2448
Programmer Sanket <55836873+DevSanket@users.noreply.github.com>
2023-05-08 19:54:12 +0530

    Working on ChatUI Buttons and Icons

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -1,12 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React from "react";
+import backgroundImage from "../assets/Images/droplet.jpeg";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ChatScreen() {
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+    <SafeAreaView edges={["right", "left", "bottom"]} style={styles.container}>
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.backgroundImage}
+      ></ImageBackground>
+      <View style={styles.inputContainer}>
+        <Button title="Image" />
+        <TextInput />
+        <Button title="Camera" />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  backgroundImage: {
+    flex: 1,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+  },
+});
